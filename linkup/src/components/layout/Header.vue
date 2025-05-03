4ee129513383c628d43f41832c86615985fa4d71
PSY <develup-psy@github.com>
2025-05-03 19:32:47 +0900

    feat: 헤더 레이아웃 추가

--- linkup/src/components/layout/Header.vue
+++ linkup/src/components/layout/Header.vue
@@ -1,31 +1,53 @@
 <script setup>
-import { useRoute } from 'vue-router'
-import { computed } from 'vue'
-import '@/assets/css/init-styles.css'
-
-const route = useRoute()
-
-// /admin 경로 여부 확인
-const isAdminRoute = computed(() => route.path.startsWith('/admin'))
+// 별도 스크립트 없음
 </script>
 
 <template>
-  <!-- 일반 사용자 헤더만 출력 -->
-  <header v-if="!isAdminRoute">
-    <div class="main-header">
-      <div class="logo">
-        <img src="@/assets/icons/logo.svg" alt="로고" />
-      </div>
-      <nav class="nav-menu">
-        <a href="#">커뮤니티</a>
-        <a href="#">모임</a>
-        <a href="#">장소</a>
-        <a href="#">사용자</a>
+  <header class="header">
+    <div class="header-inner">
+      <h1 class="logo-container">
+        <RouterLink to="/" aria-label="Linkup Home">
+          <img class="logo" src="@/assets/icons/logo.svg" alt="Linkup 로고" />
+        </RouterLink>
+      </h1>
+      <nav class="nav" aria-label="메인 네비게이션">
+        <ul class="nav-list">
+          <li><RouterLink class="nav-link" to="/likes">커뮤니티</RouterLink></li>
+          <li><RouterLink class="nav-link" to="/community">모임</RouterLink></li>
+          <li><RouterLink class="nav-link" to="/create">장소</RouterLink></li>
+          <li><RouterLink class="nav-link" to="/login">로그인/회원가입</RouterLink></li>
+        </ul>
       </nav>
     </div>
   </header>
 </template>
 
 <style scoped>
+.header {
+  @apply h-[80px] bg-indigo-100 shadow-sm flex-shrink-0 px-8;
+}
 
+.header-inner {
+  @apply h-full flex justify-between items-center max-w-full mx-auto;
+}
+
+.logo-container {
+  @apply flex items-center p-12;
+}
+
+.logo {
+  @apply w-[48px] h-[48px];
+}
+
+.nav {
+  @apply flex items-center;
+}
+
+.nav-list {
+  @apply flex space-x-12 items-center;
+}
+
+.nav-link {
+  @apply text-text-default font-medium text-bodyMd relative py-1 transition hover:text-primary-default;
+}
 </style>
